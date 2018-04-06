558c0aa9daeeb7f03e7cde66e06ce87efdf14cec
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-04-06 11:28:57 +0200

    adding aov denoising

--- noice_command_generation.py
+++ noice_command_generation.py
@@ -12,6 +12,9 @@ pixel_search_radius = 9
 pixel_neighborhood_patch_radius = 3
 temporal_range = 6
 
+#if no light aov denoising needed, just leave list empty: []
+light_aov_names = ["frontlight", "backlight"]
+
 
 filename_input_sequence_number = re.sub(r'#+',lambda m: r'{{:0{}d}}'.format(len(m.group(0))), filename_input)
 filename_output_sequence_number = re.sub(r'#+',lambda m: r'{{:0{}d}}'.format(len(m.group(0))), filename_output)
@@ -20,16 +23,21 @@ filename_output_sequence_number = re.sub(r'#+',lambda m: r'{{:0{}d}}'.format(len
 for i in range (startframe, endframe + 1):
     
     temporal_range_string = ""
+    light_aov_string = ""
     for j in range(-temporal_range/2, temporal_range/2):
         if (j==0) or (i+j < startframe) or (i+j > endframe):
             continue
         
         temporal_range_string += "-i " + filename_input_sequence_number.format(i+j) + " "
+
+    for k in light_aov_names:
+        light_aov_string += "-aov " + str(k) + " "
     
     print arnold_bin_path + "/noice.exe " \
             + "-patchradius " + str(pixel_neighborhood_patch_radius) + " " \
             + "-searchradius " + str(pixel_search_radius) + " " \
             + "-variance " + str(variance) + " " \
+            + light_aov_string + " " \
             + "-i " + filename_input_sequence_number.format(i) + " " \
             + temporal_range_string + " " \
             + "-output " + filename_output_sequence_number.format(i),
