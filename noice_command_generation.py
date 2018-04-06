23b95ef464d6632d142ccf9f33741db6cd709ea0
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-04-06 12:52:35 +0200

    restructure

--- noice_command_generation.py
+++ noice_command_generation.py
@@ -1,27 +1,23 @@
 import re
 
+# only change these variables
 filename_input = "shot.####.exr"
 filename_output = "shot_denoiced.####.exr"
 arnold_bin_path = "C:/solidangle/mtoadeploy/2018/bin"
-
 startframe = 101
 endframe = 199
-
 variance = 0.5
 pixel_search_radius = 9
 pixel_neighborhood_patch_radius = 3
 temporal_range = 6
+light_aov_names = ["frontlight", "backlight"] # if no light aov denoising needed, just leave list empty: []
 
-#if no light aov denoising needed, just leave list empty: []
-light_aov_names = ["frontlight", "backlight"]
 
 
 filename_input_sequence_number = re.sub(r'#+',lambda m: r'{{:0{}d}}'.format(len(m.group(0))), filename_input)
 filename_output_sequence_number = re.sub(r'#+',lambda m: r'{{:0{}d}}'.format(len(m.group(0))), filename_output)
 
-
 for i in range (startframe, endframe + 1):
-    
     temporal_range_string = ""
     light_aov_string = ""
     for j in range(-temporal_range/2, temporal_range/2):
